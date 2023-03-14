bfc4d1e2fa48f8caa77b02bd5db4ac699f535d0f
ronnie <rvpostha15@yahoo.com>
2023-03-14 14:31:15 -0400

    added validations to models

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,2 +1,9 @@
 class Deck < ApplicationRecord
+    #Associations
+    belongs_to :teacher
+
+    has_many :flashcards
+
+    #Validations
+    validates :title, :creator_id, presence: true
 end
--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,8 @@
 class Flashcard < ApplicationRecord
-  belongs_to :deck
+    #Associations
+    belongs_to :deck
+
+    #Validations
+    validates :front, :back, presence: true, length: { maximum: 1000 }
+    validates :deck_id, presence: true
 end
